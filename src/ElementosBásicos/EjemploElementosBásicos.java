f44d030a2034d5412cdaff791decd23e496efb29
Javi14Bueno <javi.malo14@gmail.com>
2022-09-20 20:30:38 +0200

    EjemplosLecturaPorTeclado

---
+++ src/ElementosBásicos/EjemploElementosBásicos.java
@@ -0,0 +1,27 @@
+package ElementosBásicos;
+
+public class EjemploElementosBásicos {
+    public static void main(String[] args) {
+        System.out.println("HOLA");
+        System.out.println("vamos a ver los números enteros");
+        System.out.println(1_000_000_000);
+        System.out.println("============");
+        System.out.println("Caracteres");
+        System.out.println("-----------");
+        System.out.println("Los carácteres se representan mediante comillas");
+        System.out.println("Por ejemplo: "  + 'a');
+        System.out.println("Otro ejemplo: "  + '5');
+        System.out.println("Tambíén puede ser un espacio en blanco" + ' ' + "  -.- ");
+
+
+        System.out.println("\n\nCadena de caracteres");
+        System.out.println("====================");
+        System.out.println(123 + 2 + " 123");
+        System.out.println(123 + 2 + "\t123");
+
+
+
+    }
+}
+
+
---
+++ src/ElementosBásicos/EjemplosLeerPorTeclado.java
@@ -0,0 +1,36 @@
+package ElementosBásicos;
+
+import java.util.Scanner;
+
+public class EjemplosLeerPorTeclado {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //Ahora ya podemos meter datos por teclado
+        //Pero para poder utilizarlos después en el programa
+        //Tenemos que guardarlos en una variable
+        //Las variables que guardan palabras o cadenas de texto se llaman String
+        System.out.println("Introduce tu nombre");
+        String nombre = sc.nextLine(); //escribo el nombre en el teclado y lo guardo en la variable nombre
+        System.out.println("Hola, " + nombre);
+        System.out.println("Introduce tu apellido");
+        String apellido = sc.nextLine();
+        System.out.println("Encantado señor, " + apellido);
+        System.out.println("¿Cuántos años tienes? ");
+        int edad = sc.nextInt();
+        // Este método nextInt(), tiene un peligro: si a continuación pones un nextLine() se lo salta.
+        // La manera de evitarlo es: cada vez que yo escribo nextInt(), si sé que luego voy a poner nextLine(), escribo un nextLine() adicional, sin más, de esta manera limpiamos el sc.
+        sc.nextLine();
+        System.out.println("Vaya... parece que no tienes acceso a esta página");
+        System.out.println("¿Tienes mascota?");
+        String mascota = sc.nextLine();
+        System.out.println("Vale, espere un momento");
+
+        //Vamos a meter un dato con decimales
+        System.out.println("¿Y cuál es tu patrimonio?");
+        double patrimonio = sc.nextDouble();
+        System.out.println("!" + patrimonio + "!" + "Tiene usted derecho a reclamación");
+        //Con los decimales: Cuando los escribimos en el código fuente: se pone .
+        //Cuando metemos los datos por teclado: se pone ,
+
+    }
+}
